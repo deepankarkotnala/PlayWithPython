0f8463a5bf01c681d3a191dab8a5f018c0d95366
Deepankar Kotnala <dkotnala5@gmail.com>
2020-01-12 11:13:45 +0530

    Update play_with_turtle.py

--- Turtle/play_with_turtle.py
+++ Turtle/play_with_turtle.py
@@ -1,17 +1,14 @@
-import turtle
+import turtle   # importing the turtle module
 
 my_turtle = turtle.Turtle()
 
+# Defining a function which creates a square based on the parameters "length" and "angle" passed to it
 def square(length, angle):
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
-    my_turtle.right(angle)
-    my_turtle.forward(length)
-    my_turtle.right(angle)
+    for i in range(4):
+        my_turtle.forward(length)
+        my_turtle.right(angle)
 
+# A loop for calling the square function 33 times. This will create an interesting pattern!
 for i in range(33):
     square(200, 90)
     my_turtle.right(11)
