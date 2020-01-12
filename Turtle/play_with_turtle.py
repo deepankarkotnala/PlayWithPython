4114e3ae0ec61e2e315b1607919f244f960af3f1
Deepankar Kotnala <dkotnala5@gmail.com>
2020-01-12 11:10:25 +0530

    Update play_with_turtle.py

--- Turtle/play_with_turtle.py
+++ Turtle/play_with_turtle.py
@@ -1,1 +1,17 @@
+import turtle
 
+my_turtle = turtle.Turtle()
+
+def square(length, angle):
+    my_turtle.forward(length)
+    my_turtle.right(angle)
+    my_turtle.forward(length)
+    my_turtle.right(angle)
+    my_turtle.forward(length)
+    my_turtle.right(angle)
+    my_turtle.forward(length)
+    my_turtle.right(angle)
+
+for i in range(33):
+    square(200, 90)
+    my_turtle.right(11)
