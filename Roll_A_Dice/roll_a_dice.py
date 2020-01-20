755dac359aa8a15ff13a62848c1f870ef0ef20f1
Deepankar Kotnala <dkotnala5@gmail.com>
2020-01-20 22:52:59 +0530

    Create roll_a_dice.py

---
+++ Roll_A_Dice/roll_a_dice.py
@@ -0,0 +1,9 @@
+# Import the random library
+from random import randint
+flag = True
+while flag:
+    print("You have rolled",randint(1,6))
+    print("Would you like to roll again?")
+    repeat = ("y" or "yes") in input().lower() 
+    # The user can input "Y" or 'y' or "yes" or "yeS". Because we are taking input and then converting it to lower before comparison. 
+    # Hence, it is more user friendly!
